440cd1daf0d103b471fe80e2695ce9358de181d3
JOLLA99 <yoon6148@daum.net>
2021-08-05 11:08:42 +0900

    update_210804

---
+++ Desktop/Swift_develop_study/swift/210804/210804/myTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  myTableViewCell.swift
+//  210804
+//
+//  Created by 주정윤 on 2021/08/04.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Desktop/Swift_develop_study/swift/210804/210804/myViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  210804
+//
+//  Created by 주정윤 on 2021/08/04.
+//
+
+import UIKit
+
+class myViewController: UIViewController {
+    
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
