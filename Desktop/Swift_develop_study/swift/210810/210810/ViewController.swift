9a91d0a26578091a9afd54e5214438f1b05d051e
JOLLA99 <yoon6148@daum.net>
2021-08-10 16:24:11 +0900

    210810_Add

---
+++ Desktop/Swift_develop_study/swift/210810/210810/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  210810
+//
+//  Created by 주정윤 on 2021/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func trashItem(_ sender: Any) {
+    }
+    @IBAction func airpodItem(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
