d587106e488fd6c5291c1a503868e2e5395f3bd9
JOLLA99 <yoon6148@daum.net>
2021-08-12 15:37:58 +0900

    playground_practice

---
+++ Desktop/Swift_develop_study/swift/grammer_practice.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let name : [String] = ["joo", "jeong", "han", "kim"]
+
+for stat in name{
+    print("name is \(stat)")
+}
+
+for i in stride(from: 0, to: 5, by: 1){
+    print("\(i)")
+}
+
+for i in 0...4{
+    print("\(i)")
+}
